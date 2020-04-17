e61cb03b9997e0d9b6e9e6145773d8eb1cafc45b
|wangxinleo <wangxin.leo@outlook.com>
2020-04-17 17:22:22 +0800

    2020-4-17 17:22:20

--- src/views/users/Users.vue
+++ src/views/users/Users.vue
@@ -1,24 +1,11 @@
 <template>
     <div class="users">
 <!--      面包屑导航-->
-      <el-breadcrumb separator-class="el-icon-arrow-right">
-        <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item>用户管理</el-breadcrumb-item>
-        <el-breadcrumb-item>用户列表</el-breadcrumb-item>
-      </el-breadcrumb>
+      <users-breadcrumb></users-breadcrumb>
 <!--      卡片视图区域-->
       <el-card class="box-card">
-        <el-row :gutter="20">
-          <el-col :span="8">
-<!--            搜索框-->
-            <el-input placeholder="请输入内容" v-model="query" clearable @clear="queryClick">
-              <el-button slot="append" icon="el-icon-search" @click="queryClick"></el-button>
-            </el-input>
-          </el-col>
-          <el-col :span="4">
-            <el-button type="primary" @click="dialogVisible = true">添加用户</el-button>
-          </el-col>
-        </el-row>
+<!--        顶部功能栏-->
+        <users-func @queryClick = "queryClick"></users-func>
 <!--        用户列表区域-->
         <el-table :data="usersList" border stripe>
           <el-table-column type="index"></el-table-column>
@@ -41,7 +28,7 @@
             </template>
           </el-table-column>
         </el-table>
-<!--        分页区域-->
+        <!--        分页区域-->
         <el-pagination
           @size-change="handleSizeChange"
           @current-change="handleCurrentChange"
@@ -52,31 +39,7 @@
           :total="total">
         </el-pagination>
       </el-card>
-<!--      添加用户对话框-->
-      <el-dialog
-        title="添加用户"
-        :visible.sync="dialogVisible"
-        width="60%"
-        @close="handleClose">
-        <el-form :model="addUserForm" :rules="addUserrules" ref="addUserForm" label-width="70px">
-          <el-form-item label="用户名" prop="username">
-            <el-input v-model="addUserForm.username"></el-input>
-          </el-form-item>
-          <el-form-item label="密码" prop="password">
-            <el-input v-model="addUserForm.password" type="password"></el-input>
-          </el-form-item>
-          <el-form-item label="邮箱" prop="email">
-            <el-input v-model="addUserForm.email"></el-input>
-          </el-form-item>
-          <el-form-item label="电话" prop="mobile">
-            <el-input v-model="addUserForm.mobile"></el-input>
-          </el-form-item>
-        </el-form>
-        <span slot="footer" class="dialog-footer">
-          <el-button @click="dialogVisible = false">取 消</el-button>
-          <el-button type="primary" @click="addUser">确 定</el-button>
-        </span>
-      </el-dialog>
+
 <!--      修改用户对话框-->
       <el-dialog
         title="修改用户"
@@ -103,10 +66,17 @@
 </template>
 
 <script>
-import { getUsersList, changeRowState, addUserPost, getUserFromId, updateUserSub, deleteUserById } from '../../network/users/Users'
+import UsersBreadcrumb from './usersChildren/usersBreadcrumb/UsersBreadcrumb'
+import UsersFunc from './usersChildren/usersFunc/UsersFunc'
+// 网络接口
+import { changeRowState, deleteUserById, getUserFromId, getUsersList, updateUserSub } from '../../network/users/Users'
 
 export default {
   name: 'Users',
+  components: {
+    UsersBreadcrumb,
+    UsersFunc
+  },
   data () {
     var checkEmail = (rule, value, cb) => {
       // 验证邮件的正则表达式
@@ -135,41 +105,13 @@ export default {
       pageNum: 1,
       pageSize: 10,
       total: 0,
-      // 添加用户对话框状态
-      dialogVisible: false,
       updateUserVisible: false,
-      // 添加用户的表单数据
-      addUserForm: {
-        username: '',
-        password: '',
-        email: '',
-        mobile: ''
-      },
       // 修改用户的表单数据
       updateUserForm: {
         id: '',
         username: '',
         email: '',
         mobile: ''
-      },
-      // 添加用户的input验证
-      addUserrules: {
-        username: [
-          { required: true, message: '请输入用户名', trigger: 'blur' },
-          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
-        ],
-        password: [
-          { required: true, message: '请输入密码', trigger: 'blur' },
-          { min: 6, max: 16, message: '长度在 6 到 16 个字符', trigger: 'blur' }
-        ],
-        email: [
-          { required: true, message: '请输入邮箱', trigger: 'blur' },
-          { validator: checkEmail, trigger: 'blur' }
-        ],
-        mobile: [
-          { required: true, message: '请输入电话号码', trigger: 'blur' },
-          { validator: checkMobile, trigger: 'blur' }
-        ]
       },
       // 修改用户数据的input验证
       updateUserrules: {
@@ -202,10 +144,6 @@ export default {
     changeRowStateListen (row) {
       this.changeRowState(row)
     },
-    // 搜索按钮
-    queryClick () {
-      this.getUsersList(1, 10)
-    },
     // 修改用户按钮
     update (id) {
       console.log(id)
@@ -224,37 +162,23 @@ export default {
         this.$message.info('已取消删除')
       })
     },
-    // "添加用户"弹出框关闭监听
-    handleClose () {
-      this.$refs.addUserForm.resetFields()
+    // 搜索按钮
+    queryClick (query) {
+      this.query = query
+      // 修正页码定位，防止修改后页码乱窜
+      this.pageNum = 1
+      this.pageSize = 10
+      this.getUsersList(1, 10)
     },
     // "修改用户"弹出框关闭监听
     updateUserClose () {
       this.$refs.updateUserForm.resetFields()
-    },
-    // "添加用户"中的确定按钮
-    addUser () {
-      this.$refs.addUserForm.validate(validate => {
-        if (!validate) return false
-        this.addUserPost(this.addUserForm.username, this.addUserForm.password, this.addUserForm.email, this.addUserForm.mobile)
-        this.dialogVisible = false
-      })
     },
     // "修改用户"中的确定按钮
     updateUser () {
       this.$refs.updateUserForm.validate(validate => {
         if (!validate) return false
         this.updateUserSub(this.updateUserForm.id, this.updateUserForm.email, this.updateUserForm.mobile)
-      })
-    },
-    // 获取用户列表{网络请求}
-    getUsersList (pageNum = this.pageNum, pageSize = this.pageSize, query = this.query) {
-      getUsersList(pageNum, pageSize, query).then(res => {
-        if (res.data.meta.status !== 200) return this.$message.error(res.data.meta.msg)
-        this.usersList = res.data.data.users
-        this.total = res.data.data.total
-      }).catch(err => {
-        console.log(err)
       })
     },
     // 更新用户状态{网络请求}
@@ -265,17 +189,6 @@ export default {
           return this.$message.error(res.data.meta.msg)
         }
         this.$message.success(res.data.meta.msg)
-      }).catch(err => {
-        console.log(err)
-      })
-    },
-    // 添加用户{网络请求}
-    addUserPost (username, password, email, mobile) {
-      addUserPost(username, password, email, mobile).then(res => {
-        console.log(res)
-        if (res.data.meta.status !== 201) return this.$message.error(res.data.meta.msg)
-        this.$message.success(res.data.meta.msg)
-        this.getUsersList()
       }).catch(err => {
         console.log(err)
       })
@@ -292,11 +205,12 @@ export default {
         console.log(err)
       })
     },
-    // 根据id查询用户{网络请求}
-    getUserFromId (id) {
-      getUserFromId(id).then(res => {
-        console.log(res)
-        this.updateUserForm = res.data.data
+    // 获取用户列表{网络请求}
+    getUsersList (pageNum = this.pageNum, pageSize = this.pageSize, query = this.query) {
+      getUsersList(pageNum, pageSize, query).then(res => {
+        if (res.data.meta.status !== 200) return this.$message.error(res.data.meta.msg)
+        this.usersList = res.data.data.users
+        this.total = res.data.data.total
       }).catch(err => {
         console.log(err)
       })
@@ -307,6 +221,15 @@ export default {
         if (res.data.meta.status !== 200) return this.$message.error(res.data.meta.msg)
         this.$message.success(res.data.meta.msg)
         this.getUsersList()
+      }).catch(err => {
+        console.log(err)
+      })
+    },
+    // 根据id查询用户{网络请求}
+    getUserFromId (id) {
+      getUserFromId(id).then(res => {
+        console.log(res)
+        this.updateUserForm = res.data.data
       }).catch(err => {
         console.log(err)
       })
---
+++ src/views/users/usersChildren/usersFunc/UsersFunc.vue
@@ -0,0 +1,52 @@
+<template>
+  <el-row :gutter="20">
+    <el-col :span="8">
+      <!--搜索框-->
+      <el-input placeholder="请输入内容" v-model="query" clearable @clear="queryClick">
+        <el-button slot="append" icon="el-icon-search" @click="queryClick"></el-button>
+      </el-input>
+    </el-col>
+    <el-col :span="4">
+      <!--添加用户-->
+      <el-button type="primary" @click="addUserClick">添加用户</el-button>
+    </el-col>
+    <!--添加用户弹出框-->
+    <add-user-dialog ref="addUserDialog" @addUserCancel = "addUserCancel"></add-user-dialog>
+  </el-row>
+</template>
+
+<script>
+import addUserDialog from './addUserDialog'
+export default {
+  name: 'UsersFunc',
+  components: {
+    addUserDialog
+  },
+  data () {
+    return {
+      query: '',
+      // 添加用户对话框状态
+      addUserVisible: false
+    }
+  },
+  methods: {
+    // 搜索按钮
+    queryClick () {
+      this.$emit('queryClick', this.query)
+    },
+    // 添加用户按钮
+    addUserClick () {
+      // 调用子组件方法
+      this.$refs.addUserDialog.addUserClick()
+    },
+    // 取消添加用户
+    addUserCancel () {
+      this.addUserVisible = false
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
