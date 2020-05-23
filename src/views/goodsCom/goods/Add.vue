cc384431c827d99214b5c40e6aa9fbd4e29d7e51
wangxinleo <wangxin.leo@outlook.com>
2020-05-23 22:05:44 +0800

    侧边导航栏

--- src/views/goodsCom/goods/Add.vue
+++ src/views/goodsCom/goods/Add.vue
@@ -14,8 +14,7 @@
       <el-alert title="添加商品信息" type="info" center show-icon :closable="false">
       </el-alert>
       <!-- 步骤条 -->
-      <el-steps :space="200" :active="activeIndex" finish-status="success"
-        align-center>
+      <el-steps :active="activeIndex - 0" finish-status="success" align-center>
         <el-step title="商品信息"></el-step>
         <el-step title="商品参数"></el-step>
         <el-step title="商品属性"></el-step>
@@ -23,6 +22,13 @@
         <el-step title="商品内容"></el-step>
         <el-step title="完成"></el-step>
       </el-steps>
+      <el-tabs tab-position="left" v-model="activeIndex" style="height: 200px;">
+        <el-tab-pane label="基本信息" name="0">基本信息</el-tab-pane>
+        <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
+        <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
+        <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
+        <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
+      </el-tabs>
     </el-card>
   </div>
 </template>
@@ -34,7 +40,7 @@ export default {
   components: {},
   data () {
     return {
-      activeIndex: 0
+      activeIndex: '0'
     }
   },
   computed: {},
@@ -63,6 +69,6 @@ export default {
 
 <style scoped>
 .el-steps {
-  margin: 10px 0;
+  margin: 10px auto;
 }
 </style>
