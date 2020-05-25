1102534c5e42578797941fe20b5935ed2ec6f856
wangxinleo <wangxin.leo@outlook.com>
2020-05-25 17:28:36 +0800

    基本信息商品参数

--- src/views/goodsCom/goods/Add.vue
+++ src/views/goodsCom/goods/Add.vue
@@ -25,10 +25,26 @@
       <el-form ref="addGoodFormRef" :rules="addGoodRules" :model="addGoodForm"
         label-position="top" label-width="100px">
         <el-tabs tab-position="left" v-model="activeIndex"
-          style="height: 200px;">
+          :before-leave="beforeTabsLeave" @tab-click="tabClick">
           <el-tab-pane label="基本信息" name="0">
-            <el-form-item label="活动名称">
-              <el-input v-model="addGoodForm.name"></el-input>
+            <el-form-item label="商品名称" prop="goods_name">
+              <el-input v-model="addGoodForm.goods_name"></el-input>
+            </el-form-item>
+            <el-form-item label="商品价格" prop="goods_price">
+              <el-input v-model="addGoodForm.goods_price" type="number">
+              </el-input>
+            </el-form-item>
+            <el-form-item label="商品数量" prop="goods_number">
+              <el-input v-model="addGoodForm.goods_number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品重量" prop="goods_weight">
+              <el-input v-model="addGoodForm.goods_weight"></el-input>
+            </el-form-item>
+            <el-form-item label="商品分类">
+              <el-cascader v-model="addGoodForm.goods_cat"
+                :options="categoriesData" :props="categoriesProps"
+                @change="handleChange" clearable>
+              </el-cascader>
             </el-form-item>
           </el-tab-pane>
           <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
@@ -42,28 +58,100 @@
 </template>
 
 <script>
-
+import { getCategories } from 'network/goodsCom/Categories'
+import { getAttributesById } from 'network/goodsCom/Params'
 export default {
   name: 'GoodsAdd',
   components: {},
   data () {
     return {
       activeIndex: '0',
+      // 商品分类的表单数据对象
       addGoodForm: {
-        name: ''
+        goods_name: '',
+        goods_price: 0,
+        goods_number: 0,
+        goods_weight: 0,
+        goods_cat: [],
+        attrs: []
       },
-      addGoodRules: []
+      addGoodRules: {
+        goods_name: [
+          { required: true, message: '请输入商品名称', trigger: 'blur' }
+        ],
+        goods_price: [
+          { required: true, message: '请输入商品价格', trigger: 'blur' }
+        ],
+        goods_number: [
+          { required: true, message: '请输入商品数量', trigger: 'blur' }
+        ],
+        goods_weight: [
+          { required: true, message: '请输入商品重量', trigger: 'blur' }
+        ]
+      },
+      categoriesData: [],
+      categoriesProps: {
+        expandTrigger: 'hover',
+        label: 'cat_name',
+        value: 'cat_id',
+        children: 'children'
+      },
+      // 动态数据列表
+      manyTableData: []
     }
   },
-  computed: {},
+  computed: {
+    cateId () {
+      if (this.addGoodForm.goods_cat.length === 3) {
+        return this.addGoodForm.goods_cat[2]
+      }
+      return null
+    }
+  },
   // 监控data中的数据变化
   watch: {},
   methods: {
-
+    // 点击标签页
+    tabClick () {
+      console.log(this.activeIndex)
+      if (this.activeIndex === '1') {
+        this.getAttributesById(this.cateId, 'many')
+      }
+    },
+    // 标签页切换前
+    beforeTabsLeave (activeName, oldActiveName) {
+      if (oldActiveName === '0' && this.addGoodForm.goods_cat.length !== 3) {
+        this.$message.error('请选择商品分类')
+        return false
+      }
+    },
+    // 级联选择器发生变化
+    handleChange () {
+      console.log(this.addGoodForm.goods_cat)
+    },
+    // 获取商品分类数据
+    getCategories (type = '', pageNum = '', pageSize = '') {
+      getCategories(type, pageNum, pageSize).then(res => {
+        if (res.data.meta.status !== 200) return this.$message.error(res.data.meta.msg)
+        this.$message.success(res.data.meta.msg)
+        this.categoriesData = res.data.data
+      }).catch(err => {
+        console.log(err)
+      })
+    },
+    // 获取参数数据
+    getAttributesById (id, sel) {
+      getAttributesById(id, sel).then(res => {
+        if (res.data.meta.status !== 200) return this.$message.error(res.data.meta.msg)
+        this.manyTableData = res.data.data
+      }).catch(err => {
+        console.log(err)
+      })
+    }
   },
   // 生命周期 - 创建完成（可以访问当前this实例）
   created () {
-
+    this.getCategories()
   },
   // 生命周期 - 挂载完成（可以访问DOM元素）
   mounted () {
@@ -82,5 +170,8 @@ export default {
 <style scoped>
 .el-steps {
   margin: 10px auto;
+}
+.el-cascader {
+  width: 50%;
 }
 </style>
