253d10863c0660783e5e4026889813f805be7a03
wangxinleo <wangxin.leo@outlook.com>
2020-05-27 17:01:45 +0800

    按需移除console

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,9 @@
+// 这是项目发布阶段需要用到的babel插件
+const prodPlugins = []
+if (process.env.NODE_ENV === 'production') {
+  prodPlugins.push('transform-remove-console')
+}
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
@@ -10,6 +16,7 @@ module.exports = {
         styleLibraryName: 'theme-chalk'
       }
     ],
-    'transform-remove-console'
+    // 发布产品时候的插件数组
+    ...prodPlugins
   ]
 }
