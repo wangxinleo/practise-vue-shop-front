929a0dee1c96bcf62cc054b75d58f49e90861b7e
wangxinleo <wangxin.leo@outlook.com>
2020-05-28 16:32:08 +0800

    配置server

--- src/network/home/Home.js
+++
@@ -1,8 +0,0 @@
-import { request } from '../request'
-
-export function getHomeList () {
-  return request({
-    url: '/menus',
-    method: 'get'
-  })
-}
--- src/network/report/report.js
+++
@@ -1,8 +0,0 @@
-import { request } from '../request'
-
-export function getMap () {
-  return request({
-    url: 'reports/type/1',
-    method: 'get'
-  })
-}
